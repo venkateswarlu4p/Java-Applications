6baa54bd2dea072973c52303b4ef201deac68300
venkateswarlu4p <125424850+venkateswarlu4p@users.noreply.github.com>
2026-02-02 01:10:32 -0500

    Delete src directory

--- src/main/java/com/aws/lambda/OrderHandler.java
+++
@@ -1,6 +0,0 @@
-package com.aws.lambda;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class OrderHandler extends SpringBootRequestHandler<String,Object> {
-}
